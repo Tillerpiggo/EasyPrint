ee8f3fa0c546003073ffdfa253afde40eeff017c
Tyler <tjr20@uw.edu>
2023-10-31 09:45:55 -0700

    added depedancy in json file

---
+++ easyprint/out/jest.config.js
@@ -0,0 +1,8 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const jest_config_1 = require("jest-config");
+const config = {
+    moduleFileExtensions: [...jest_config_1.defaults.moduleFileExtensions, 'mts'],
+};
+exports.default = config;
+//# sourceMappingURL=jest.config.js.map
--- easyprint/out/test/suite/extension.test.js
+++ easyprint/out/test/suite/extension.test.js
@@ -1,19 +1,23 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const APIController_1 = require("../../APIController");
+//import * as assert from 'assert';
+//import { generateResponse } from '../../APIController';
+const expect = require('@jest/globals');
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-const vscode = require("vscode");
+//import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
-suite('Extension Test Suite', () => {
-    vscode.window.showInformationMessage('Start all tests.');
-    (0, APIController_1.generateResponse)("Please send me a message that says hello")
-        .then((response) => {
+const { generateResponse } = require('../../APIController');
+//suite('Extension Test Suite', () => {
+//vscode.window.showInformationMessage('Start all tests.');
+test('Tests basic response from AI API', async () => {
+    try {
+        const response = await generateResponse("Please send me a message that says hello");
         // Print the generated response
-        console.log(response);
-    })
-        .catch((error) => {
+        expect(response).toBe("Hello!");
+    }
+    catch (error) {
         console.error('Error:', error);
-    });
+    }
 });
+//});
 //# sourceMappingURL=extension.test.js.map
