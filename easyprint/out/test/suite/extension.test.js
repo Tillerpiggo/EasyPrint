dd8e627824e2e0f129eccb3fb7651333c2ad5466
Tyler <tjr20@uw.edu>
2023-10-31 08:12:54 -0700

    added global typescript command

--- easyprint/out/test/suite/extension.test.js
+++ easyprint/out/test/suite/extension.test.js
@@ -1,15 +1,19 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const assert = require("assert");
+const APIController_1 = require("../../APIController");
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 const vscode = require("vscode");
 // import * as myExtension from '../../extension';
 suite('Extension Test Suite', () => {
     vscode.window.showInformationMessage('Start all tests.');
-    test('Sample test', () => {
-        assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-        assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+    (0, APIController_1.generateResponse)("Please send me a message that says hello")
+        .then((response) => {
+        // Print the generated response
+        console.log(response);
+    })
+        .catch((error) => {
+        console.error('Error:', error);
     });
 });
 //# sourceMappingURL=extension.test.js.map
--- easyprint/src/test/suite/extension.test.ts
+++ easyprint/src/test/suite/extension.test.ts
@@ -1,5 +1,5 @@
 import * as assert from 'assert';
-
+import { generateResponse } from '../../APIController';
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
@@ -8,8 +8,12 @@ import * as vscode from 'vscode';
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+	generateResponse("Please send me a message that says hello")
+		.then((response) => {
+			// Print the generated response
+			console.log(response);
+		})
+		.catch((error) => {
+			console.error('Error:', error);
+		});
 });
