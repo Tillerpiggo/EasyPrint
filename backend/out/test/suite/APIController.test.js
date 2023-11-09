8326396514cd0002afae3a8a2334d851c32aa7be
tjr20 <tjr20@uw.edu>
2023-11-08 19:59:10 -0800

    fixed more testing issues

--- backend/out/test/suite/APIController.test.js
+++ backend/out/test/suite/APIController.test.js
@@ -12,13 +12,4 @@ test('Tests basic response from AI API', async () => {
         console.error('Error:', error);
     }
 });
-test('Tests another basic response from AI API', async () => {
-    try {
-        const response = await apiController.generateResponse("Please send me a message that says goodbye with no punctuation and all lower case");
-        expect(response).toBe("goodbye");
-    }
-    catch (error) {
-        console.error('Error:', error);
-    }
-});
 //# sourceMappingURL=APIController.test.js.map
